3f0ad88e13e34208217687c7d1a9f2c02721db65
maxyee <mdjulhashossainmohon@gmail.com>
2017-11-12 01:14:27 +0600

    Learned how to add style and how to add html into body part using reactjs

--- reactApp/App.jsx
+++ reactApp/App.jsx
@@ -4,9 +4,23 @@ class App extends React.Component
 {
   render()
   {
+    var i = 1;
+
+    var mystyle = {
+      color: 'red',
+      fontSize:20
+    }
+
     return(
       <div>
+          <h1>Header</h1>
+          <h2>Content</h2>
+          <p data-myattribute="somevalue">This is Content of paragraph</p>
           Hello World!!!
+          <h1>{1+1}</h1>
+          <h2>{i == 1 ? 'True!' : 'False'}</h2>
+          <p >Hello Julhas How are you :P</p>
+          <p style = {mystyle} >This is how i add style into ReactJS</p>
       </div>
     );
   }
